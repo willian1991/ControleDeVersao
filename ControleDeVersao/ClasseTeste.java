d7f7510e1c254f17cb6415bfb677adf4cf37e75d
willianSilva <williansilva@computacao.ufla.br>
2014-12-04 13:50:41 -0200

    Versão Inicial

---
+++ ControleDeVersao/ClasseTeste.java
@@ -0,0 +1,19 @@
+package ControleDeVersao;
+
+public class ClasseTeste {
+	int A1;
+	float A2;
+	public int getA1() {
+		return A1;
+	}
+	public void setA1(int a1) {
+		A1 = a1;
+	}
+	public float getA2() {
+		return A2;
+	}
+	public void setA2(float a2) {
+		A2 = a2;
+	}
+
+}
