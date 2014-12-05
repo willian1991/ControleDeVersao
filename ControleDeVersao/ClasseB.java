e69ded12cacc0b403a7d87bd935baea2ee8d1090
willianSilva <williansilva@computacao.ufla.br>
2014-12-05 18:44:09 -0200

    Versão 1.3 Principal (Controle de versão)

---
+++ ControleDeVersao/ClasseB.java
@@ -0,0 +1,20 @@
+package ControleDeVersao;
+
+public class ClasseB {
+	int B1;
+	float B2;
+	
+	public int getB1() {
+		return B1;
+	}
+	public void setB1(int b1) {
+		B1 = b1;
+	}
+	public float getB2() {
+		return B2;
+	}
+	public void setB2(float b2) {
+		B2 = b2;
+	}
+
+}
